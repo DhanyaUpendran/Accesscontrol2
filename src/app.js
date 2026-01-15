631bade91901e81bc4903161ee184640cf76733a
Dhanya <dhanya@dell.com>
2026-01-15 13:33:23 +0530

    changes updated on app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 import express from "express";
+import dotenv from "dotenv";
 import cors from "cors";
+import connectDB from "./config/db.js";
+dotenv.config();
+connectDB();
+
+// Routes
+import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
+import adminRoutes from "./routes/admin.routes.js";
+
+
+
 
 const isDev = process.env.NODE_ENV === "development";
-
+const app = express();
 const allowedOrigins = isDev
   ? ["http://localhost:5173"]
   : ["https://accesscontrol2-frontend.vercel.app"];
@@ -23,3 +35,13 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization"]
   })
 );
+app.use(express.json());
+
+// API Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/admin", adminRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+export default app;
